55a5958c05c17065b7c109a154cc49b1a8484930
Arshit Rawat <arshitrawat2704@gmail.com>
2024-11-28 19:00:30 +0530

    8_01

---
+++ part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Cubes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            String input = scanner.nextLine();
+            if(input.equals("end")){
+                break;
+            } else{
+                int num = Integer.valueOf(input);
+                System.out.println(cube(num));
+            }
+        }
+
+    }
+    
+    public static int cube(int num){
+        return num*num*num;
+    }
+}
